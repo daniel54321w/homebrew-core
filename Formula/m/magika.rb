91a067b2bd16ba5654bc3eb98b0c4eb157e9e46a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-25 08:27:32 +0000

    magika 1.0.2

--- Formula/m/magika.rb
+++ Formula/m/magika.rb
@@ -1,8 +1,8 @@
 class Magika < Formula
   desc "Fast and accurate AI powered file content types detection"
   homepage "https://securityresearch.google/magika/"
-  url "https://github.com/google/magika/archive/refs/tags/cli/v1.0.1.tar.gz"
-  sha256 "a1574996fadb4fc262e0d652dc9b8d6a837c0911e620245afe9d1ea3881ebfd7"
+  url "https://github.com/google/magika/archive/refs/tags/cli/v1.0.2.tar.gz"
+  sha256 "bae42b31c8f419f34043cc2cf26fa42d2ade7f7c91e2fb54919914432f799699"
   license "Apache-2.0"
 
   head "https://github.com/google/magika.git", branch: "main"
@@ -23,11 +23,13 @@ class Magika < Formula
 
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
+  depends_on "onnxruntime"
   depends_on "openssl@3"
 
   def install
     ENV["OPENSSL_NO_VENDOR"] = "1"
     ENV["OPENSSL_DIR"] = Formula["openssl@3"].opt_prefix
+    ENV["ORT_LIB_PATH"] = Formula["onnxruntime"].opt_lib
 
     system "cargo", "install", *std_cargo_args(path: "rust/cli")
   end
